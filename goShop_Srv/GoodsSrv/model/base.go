fda549fcc3ef48a88b9f49886ee215100426abe8
luxe7 <luxe7@foxmail.com>
2024-02-28 12:00:29 +0800

    create model of goodSrv

--- goShop_Srv/GoodsSrv/model/base.go
+++ goShop_Srv/GoodsSrv/model/base.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"database/sql/driver"
+	"encoding/json"
 	"gorm.io/gorm"
 	"time"
 )
@@ -11,4 +13,14 @@ type BaseModel struct {
 	UpdatedAt time.Time `gorm:"column:update_time"`
 	DeletedAt gorm.DeletedAt
 	IsDeleted bool
+}
+type GormList []string
+
+func (g GormList) Value() (driver.Value, error) {
+	return json.Marshal(g)
+}
+
+// Scan 实现 sql.Scanner 接口，Scan 将 value 扫描至 Jsonb
+func (g *GormList) Scan(value interface{}) error {
+	return json.Unmarshal(value.([]byte), &g)
 }
