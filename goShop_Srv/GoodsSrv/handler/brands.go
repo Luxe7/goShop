5eccb4ad282cd8ca9a3ffc2427b339157ca4619a
Luxe7 <luxe7@foxmail.com>
2024-02-29 19:38:28 +0800

    add handler of banner and brands

--- goShop_Srv/GoodsSrv/handler/brands.go
+++ goShop_Srv/GoodsSrv/handler/brands.go
@@ -1,7 +1,74 @@
 package handler
 
-// 品牌和轮播图
-BrandList(context.Context, *BrandFilterRequest) (*BrandListResponse, error)
-CreateBrand(context.Context, *BrandRequest) (*BrandInfoResponse, error)
-DeleteBrand(context.Context, *BrandRequest) (*emptypb.Empty, error)
-UpdateBrand(context.Context, *BrandRequest) (*emptypb.Empty, error)
+import (
+	"context"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"goShop/GoodsSrv/global"
+	"goShop/GoodsSrv/model"
+	"goShop/GoodsSrv/proto"
+)
+
+func (s *GoodsServer) BrandList(ctx context.Context, req *proto.BrandFilterRequest) (*proto.BrandListResponse, error) {
+	resp := &proto.BrandListResponse{}
+
+	var brands []model.Brands
+	result := global.DB.Scopes(Paginate(int(req.Pages), int(req.PagePerNums))).Find(&brands)
+	if result.Error != nil {
+		zap.S().Error("[BrandList]", result.Error)
+		return nil, status.Error(codes.Internal, "DB find fail")
+	}
+	var total int64
+	global.DB.Model(&model.Brands{}).Count(&total)
+
+	var brandResponses []*proto.BrandInfoResponse
+	for _, brand := range brands {
+		brandResponses = append(brandResponses, &proto.BrandInfoResponse{
+			Id:   brand.ID,
+			Name: brand.Name,
+			Logo: brand.Logo,
+		})
+	}
+	resp.Total = int32(total)
+	resp.Data = brandResponses
+	return resp, nil
+
+}
+func (s *GoodsServer) CreateBrand(ctx context.Context, req *proto.BrandRequest) (*proto.BrandInfoResponse, error) {
+	resp := &proto.BrandInfoResponse{}
+	if result := global.DB.First(&model.Brands{}, req.Id); result.RowsAffected != 0 {
+		return nil, status.Error(codes.InvalidArgument, "brand is exist")
+	}
+	brand := model.Brands{
+		Name: req.Name,
+		Logo: req.Logo,
+	}
+	global.DB.Save(brand)
+	resp.Id = brand.ID
+	return resp, nil
+
+}
+func (s *GoodsServer) DeleteBrand(ctx context.Context, req *proto.BrandRequest) (*emptypb.Empty, error) {
+	if result := global.DB.Delete(&model.Brands{}, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "brand is not exist")
+	}
+	return nil, nil
+}
+func (s *GoodsServer) UpdateBrand(ctx context.Context, req *proto.BrandRequest) (*emptypb.Empty, error) {
+	brand := model.Brands{}
+	if result := global.DB.First(&brand, req.Id); result.RowsAffected != 0 {
+		return nil, status.Error(codes.InvalidArgument, "brand is exist")
+	}
+	if req.Name != "" {
+		brand.Name = req.Name
+	}
+	if req.Logo != "" {
+		brand.Logo = req.Logo
+	}
+	global.DB.Save(&brand)
+	return nil, nil
+}
--- goShop_Srv/GoodsSrv/model/main/main.go
+++ goShop_Srv/GoodsSrv/model/main/main.go
@@ -1,45 +1,24 @@
 package main
 
 import (
-	"crypto/md5"
-	"crypto/sha512"
-	"encoding/hex"
-	"fmt"
-	"github.com/anaskhan96/go-password-encoder"
-	"goShop/GoodsSrv/model"
+	"log"
+	"os"
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
-	"io"
-	"log"
-	"os"
-	"strings"
-	"time"
+
+	"goShop/GoodsSrv/model"
 )
 
 /*
 对用户密码要进行加密，防止彩虹表撞库，我们要对密码进行加盐
 */
 
-func genMd5(code string) string {
-	Md5 := md5.New()
-	_, _ = io.WriteString(Md5, code)
-	return hex.EncodeToString(Md5.Sum(nil))
-}
-
 func main() {
-
-	// Using custom options
-	options := &password.Options{16, 100, 32, sha512.New}
-	salt, encodedPwd := password.Encode("generic password", options)
-	genPassword := fmt.Sprintf("pbkdf2-sha512$%s$%s", salt, encodedPwd)
-
-	passwordInfo := strings.Split(genPassword, "$")
-	check := password.Verify("generic password", passwordInfo[1], passwordInfo[2], options)
-	fmt.Println(check) // true
-
-	dsn := "root:123456@tcp(127.0.0.1:3306)/goshop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:123456@tcp(127.0.0.1:3306)/goshop_goods_srv?charset=utf8mb4&parseTime=True&loc=Local"
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
@@ -59,13 +38,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&model.User{})
-	for i := 0; i < 10; i++ {
-		user := model.User{
-			Nickname: fmt.Sprintf("bobby%d", i),
-			Mobil:    fmt.Sprintf("1878222222%d", i),
-			Password: "newPassword",
-		}
-		db.Save(&user)
-	}
+	db.AutoMigrate(&model.Category{}, &model.Brands{}, &model.GoodsCategoryBrand{}, &model.Banner{}, &model.Goods{})
 }
