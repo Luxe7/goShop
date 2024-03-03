06720db29a19edbc438cde2828e3e07a940da5b1
Luxe7 <luxe7@foxmail.com>
2024-03-03 18:10:15 +0800

    fix consul health check problem and optime consul register

--- goShop_Srv/InventorySrv/main.go
+++ goShop_Srv/InventorySrv/main.go
@@ -3,49 +3,57 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/apache/rocketmq-client-go/v2"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 
-	uuid "github.com/satori/go.uuid"
+	"github.com/apache/rocketmq-client-go/v2/consumer"
+	"github.com/satori/go.uuid"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 
-	"goShop/GoodsSrv/global"
-	"goShop/GoodsSrv/handler"
-	"goShop/GoodsSrv/initialize"
-	"goShop/GoodsSrv/proto"
-	"goShop/GoodsSrv/utils"
-	"goShop/GoodsSrv/utils/register/consul"
+	"goShop/InventorySrv/global"
+	"goShop/InventorySrv/handler"
+	"goShop/InventorySrv/initialize"
+	"goShop/InventorySrv/proto"
+	"goShop/InventorySrv/utils"
+	"goShop/InventorySrv/utils/register/consul"
 )
 
 func main() {
-	IP := flag.String("ip", "10.66.58.204", "IP地址")
-	Port := flag.Int("port", 0, "端口号")
+	IP := flag.String("ip", "0.0.0.0", "ip地址")
+	Port := flag.Int("port", 50053, "端口号")
 
+	//初始化
 	initialize.InitLogger()
 	initialize.InitConfig()
 	initialize.InitDB()
 	zap.S().Info(global.ServerConfig)
 
 	flag.Parse()
+
 	if *Port == 0 {
 		*Port, _ = utils.GetFreePort()
 	}
-	zap.S().Info("Ip:", *IP)
-	zap.S().Info("Port:", *Port)
+	zap.S().Info("ip: ", *IP)
+	zap.S().Info("port: ", *Port)
 
 	server := grpc.NewServer()
-	proto.RegisterGoodsServer(server, &handler.GoodsServer{})
+	proto.RegisterInventoryServer(server, &handler.InventoryServer{})
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *IP, *Port))
 	if err != nil {
 		panic("failed to listen:" + err.Error())
 	}
 
+	//注册服务健康检查
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+
+	//启动服务
 	go func() {
 		err = server.Serve(lis)
 		if err != nil {
@@ -53,8 +61,6 @@ func main() {
 		}
 	}()
 
-	//注册服务健康检查
-	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
 	//服务注册
 	registerClient := consul.NewRegistryClient(global.ServerConfig.ConsulInfo.Host, global.ServerConfig.ConsulInfo.Port)
 	serviceId := fmt.Sprintf("%s", uuid.NewV4())
@@ -64,10 +70,23 @@ func main() {
 	}
 	zap.S().Debugf("启动服务器, 端口： %d", *Port)
 
+	//监听库存归还topic
+	c, _ := rocketmq.NewPushConsumer(
+		consumer.WithNameServer([]string{"192.168.0.104:9876"}),
+		consumer.WithGroupName("mxshop-inventory"),
+	)
+
+	if err := c.Subscribe("order_reback", consumer.MessageSelector{}, handler.AutoReback); err != nil {
+		fmt.Println("读取消息失败")
+	}
+	_ = c.Start()
+	//不能让主goroutine退出
+
 	//接收终止信号
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
+	_ = c.Shutdown()
 	if err = registerClient.DeRegister(serviceId); err != nil {
 		zap.S().Info("注销失败:", err.Error())
 	} else {
--- goShop_Srv/UserSrv/main.go
+++ goShop_Srv/UserSrv/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"goShop/UserSrv/utils/register/consul"
 	"net"
 	"os"
 	"os/signal"
@@ -17,7 +18,6 @@ import (
 	"goShop/UserSrv/proto"
 	"goShop/UserSrv/utils"
 
-	"github.com/hashicorp/consul/api"
 	uuid "github.com/satori/go.uuid"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -30,6 +30,7 @@ func main() {
 	initialize.InitLogger()
 	initialize.InitConfig()
 	initialize.InitDB()
+	zap.S().Info(global.ServerConfig)
 
 	flag.Parse()
 	if *Port == 0 {
@@ -40,42 +41,6 @@ func main() {
 
 	server := grpc.NewServer()
 	proto.RegisterUserServer(server, &handler.UserServer{})
-
-	//注册服务健康检查
-	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
-	//服务注册
-	cfg := api.DefaultConfig()
-	cfg.Address = fmt.Sprintf("%s:%d", global.ServerConfig.ConsulInfo.Host,
-		global.ServerConfig.ConsulInfo.Port)
-
-	client, err := api.NewClient(cfg)
-	if err != nil {
-		panic(err)
-	}
-	//生成对应的检查对象
-	// check := &api.AgentServiceCheck{
-	// 	GRPC:                           fmt.Sprintf("%s:%d", "10.66.58.204", *Port),
-	// 	Timeout:                        "5s",
-	// 	Interval:                       "5s",
-	// 	DeregisterCriticalServiceAfter: "60s",
-	// }
-
-	//生成注册对象
-	serviceID := fmt.Sprintf("%s", uuid.NewV4())
-	registration := &api.AgentServiceRegistration{
-		Name:    global.ServerConfig.Name,
-		ID:      serviceID,
-		Port:    *Port,
-		Tags:    []string{"user", "srv"},
-		Address: "10.66.58.204",
-		// Check:   check,
-	}
-	//1. 如何启动两个服务
-	//2. 即使我能够通过终端启动两个服务，但是注册到consul中的时候也会被覆盖
-	err = client.Agent().ServiceRegister(registration)
-	if err != nil {
-		panic(err)
-	}
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *IP, *Port))
 	if err != nil {
@@ -89,13 +54,24 @@ func main() {
 		}
 	}()
 
+	//注册服务健康检查
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+	//服务注册
+	registerClient := consul.NewRegistryClient(global.ServerConfig.ConsulInfo.Host, global.ServerConfig.ConsulInfo.Port)
+	serviceId := fmt.Sprintf("%s", uuid.NewV4())
+	err = registerClient.Register(global.ServerConfig.Host, *Port, global.ServerConfig.Name, global.ServerConfig.Tags, serviceId)
+	if err != nil {
+		zap.S().Panic("服务注册失败:", err.Error())
+	}
+	zap.S().Debugf("启动服务器, 端口： %d", *Port)
+
 	//接收终止信号
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	if err = client.Agent().ServiceDeregister(serviceID); err != nil {
-		zap.S().Info("注销失败")
+	if err = registerClient.DeRegister(serviceId); err != nil {
+		zap.S().Info("注销失败:", err.Error())
 	} else {
-		zap.S().Info("注销成功")
+		zap.S().Info("注销成功:")
 	}
 }
